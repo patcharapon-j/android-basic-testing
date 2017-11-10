bed60bc561f49cd5b11c9f39034b8a8aea249197
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-10 11:33:03 +0700

    Follow Lecture add Calculator class and test

---
+++ app/src/main/java/cc/somkiat/basicunittesting/Calculator.java
@@ -0,0 +1,11 @@
+package cc.somkiat.basicunittesting;
+
+/**
+ * Created by patcharaponjoksamut on 10/11/2017 AD.
+ */
+
+class Calculator {
+    public int plus(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+}
