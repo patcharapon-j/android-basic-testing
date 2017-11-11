c6ea56cb2751b72687ea322f3582a51595baee8d
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-11 17:26:01 +0700

    Add UI Test Espresso

--- app/src/androidTest/java/cc/somkiat/basicunittesting/Controller/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/Controller/MainActivityTest.java
@@ -19,16 +19,20 @@ import org.junit.runner.RunWith;
 
 import cc.somkiat.basicunittesting.R;
 
+import static android.app.PendingIntent.getActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -45,8 +49,48 @@ public class MainActivityTest {
     }
 
     @Test
-    public void saveTest() {
-        
+    public void saveSuccessfulTest() {
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText("John Smith"));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("test@gmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+        onView(withText("Save Successful")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void saveFailNoName() {
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText(""));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("test@gmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+        onView(withText("Name is Empty String")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void saveFailNoEmail() {
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText("John Smith"));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText(""));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+        onView(withText("Email is Empty")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void saveFailInvalidName() {
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText("Sam1337"));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("test@gmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+        onView(withText("Name contain non Alphabet Characters")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void saveFailInvalidEmail() {
+        onView(withId(R.id.revertButton)).perform(scrollTo(), click());
+        onView(withId(R.id.userNameInput)).perform(scrollTo() ,replaceText("John Smith"));
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("gmail.com"));
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+        onView(withText("Email is Invalid")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 
 }
