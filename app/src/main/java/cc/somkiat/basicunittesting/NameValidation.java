c3b6aa420e29a28c0eca4f3caf7ac5b9b2c0ce60
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-11 14:12:37 +0700

    Add Name Validation Class

---
+++ app/src/main/java/cc/somkiat/basicunittesting/NameValidation.java
@@ -0,0 +1,11 @@
+package cc.somkiat.basicunittesting;
+
+/**
+ * Created by patcharaponjoksamut on 11/11/2017 AD.
+ */
+
+class NameValidation {
+    public Boolean validate(String inputName) {
+        return false;
+    }
+}
