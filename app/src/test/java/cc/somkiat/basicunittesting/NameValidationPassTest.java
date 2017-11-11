80810212669c3a3b7a812394b0bdca777114b84d
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-11 15:41:26 +0700

    Add Name is Single Word Test Case

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationPassTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationPassTest.java
@@ -2,6 +2,8 @@ package cc.somkiat.basicunittesting;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -14,6 +16,15 @@ public class NameValidationPassTest {
     public void NameIsSingleCharacter() {
         NameValidation nameValidation = new NameValidation();
         String inputName = "S";
+        ValidationResult result = nameValidation.validate(inputName);
+
+        assertTrue(result.getResult());
+    }
+
+    @Test
+    public void NameIsSingleWord() {
+        NameValidation nameValidation = new NameValidation();
+        String inputName = "Smith";
         ValidationResult result = nameValidation.validate(inputName);
 
         assertTrue(result.getResult());
