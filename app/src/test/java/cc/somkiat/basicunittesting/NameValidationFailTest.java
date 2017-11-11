12d2dd18d34dfa50eda65e2c5cc56c3bcf0022c3
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-11 14:10:51 +0700

    Add new Test Case
    For NameValidationFailTest
    - Name Is null

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
@@ -1,8 +1,21 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+
 /**
  * Created by patcharaponjoksamut on 11/11/2017 AD.
  */
 
 public class NameValidationFailTest {
+
+    @Test
+    public void NameIsNull() {
+        NameValidation nameValidation = new NameValidation();
+        String inputName = null;
+        Boolean result = nameValidation.validate(inputName);
+        assertFalse("Name is NULL must return FALSE", result);
+    }
+
 }
